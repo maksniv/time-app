bef264336ff8e757bcf6b32f9b12b8fdeee40ac1
maksim niveykin <maksniv@gmail.com>
2024-02-27 18:00:46 +0300

    app deploy

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # time-app
+
+Приложение состоит из backend, frontend и database. Реализовано:
+
+1. SPA на VUE.
+2. Backend на Express JS.
+3. Все обернуто в Docker.
+
+## Стек технологий
+
+- VUE
+- Express JS
+- Docker
+- Docker Compose
---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Time :showSavedTimes="true" />
+</template>
+
+<script setup>
+import Time from './components/Time.vue';
+</script>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  margin: 100px auto;
+}
+
+.green {
+  color: hsla(160, 100%, 37%, 1);
+}
+</style>
